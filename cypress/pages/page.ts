7ca102dd394cd2dcf6264265e1efc5ea9e9aedf1
haite4 <ugrayvcsgo137@gmail.com>
2024-12-23 14:48:45 +0000

    Update files in page.ts

--- cypress/pages/page.ts
+++ cypress/pages/page.ts
@@ -6,10 +6,6 @@ class Page{
     get tendersButton() {
         return cy.get('[class*="Navbar_link"]').contains("Тендери");
     }
-
-    get announcementLink(){
-        return cy.get('[class*="Navbar_link"]').contains("Оголошення")
-    }
 }
 
 
